df99187d1abafa1640b57de68b96d174ff3b1d32
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-07-28 23:01:13 +0200

    Added DataProxyFactory to sample project

--- samples/01-books/BooksDataProxy.dpr
+++ samples/01-books/BooksDataProxy.dpr
@@ -4,7 +4,8 @@ uses
   Vcl.Forms,
   Form.Main in 'Form.Main.pas' {Form1},
   Data.Proxy.Book in 'Data.Proxy.Book.pas',
-  Data.DataProxy in '..\base\Data.DataProxy.pas';
+  Data.DataProxy in '..\base\Data.DataProxy.pas',
+  Data.DataProxy.Factory in '..\base\Data.DataProxy.Factory.pas';
 
 {$R *.res}
 
