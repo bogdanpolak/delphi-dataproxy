2f271d8bfac4f5676d70acb8b395e708bcbd8e79
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-08-09 17:02:36 +0200

    Removed old factory

--- samples/base/Data.DataProxy.Factory.pas
+++ samples/base/Data.DataProxy.Factory.pas
@@ -12,43 +12,12 @@ uses
   FireDAC.Comp.Client;
 
 type
-  TDatasetProxyClass = class of TDatasetProxy;
-
-  TDataProxyFactory = class
-    class function CreateAndOpenProxy(AClass: TDatasetProxyClass;
-      Owner: TComponent; Connection: TFDConnection; const ASqlStatement: string): TDatasetProxy; overload;
-    class function CreateAndOpenProxy(AClass: TDatasetProxyClass;
-      Owner: TComponent; Connection: TFDConnection; const ASqlStatement: string;  const AParams: array of Variant): TDatasetProxy; overload;
-  end;
-
-type
   TDataProxyFactoryNew = class
     class function CreateProxy<T: TDataSetProxy> (Owner: TComponent;
       ADataSet: TDataSet): T;
   end;
 
 implementation
-
-class function TDataProxyFactory.CreateAndOpenProxy(AClass: TDatasetProxyClass;
-  Owner: TComponent; Connection: TFDConnection; const ASqlStatement: string): TDatasetProxy;
-begin
-  Result := CreateAndOpenProxy(AClass, Owner, Connection, ASqlStatement, []);
-end;
-
-class function TDataProxyFactory.CreateAndOpenProxy(AClass: TDatasetProxyClass;
-  Owner: TComponent; Connection: TFDConnection; const ASqlStatement: string;
-  const AParams: array of Variant): TDatasetProxy;
-var
-  ds: TDataSet;
-begin
-  Result := AClass.Create(Owner);
-  Connection.ExecSQL('SELECT ISBN, Title, Authors, Status, ' +
-    'ReleseDate, Pages, Price, Currency, Imported, Description FROM Books', ds);
-  Result.ConnectWithDataSet(ds);
-  Result.Open;
-end;
-
-{ TDataProxyFactoryNew }
 
 class function TDataProxyFactoryNew.CreateProxy<T>(Owner: TComponent;
   ADataSet: TDataSet): T;
