c99b8323ba9b6a7c10056716c7ca96f597e14c5e
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-01-20 23:11:10 +0000

    Finished all refactorings

--- tests/Test.ProxyGenerator.pas
+++ tests/Test.ProxyGenerator.pas
@@ -31,22 +31,24 @@ type
     procedure TearDown;
   published
     // ---
-    procedure Test_UnitHeader_IsEmpty;
-    procedure Test_UsesSection;
+    procedure GenUnitHeader_IsEmpty;
+    procedure GenUsesSection;
     // ---
     procedure GenClassFields_Integer;
     procedure GenClassFields_Integer_LowerCaseStyle;
     procedure GenClassFields_String;
+    procedure GenProperty_Date;
+    procedure GenFieldAssigment_Currency;
     // ---
-    procedure Test_ClassDeclaration_DataSetNil;
-    procedure Test_ClassDeclaration_DataSetOneField;
-    procedure GenerateClass_TwoFields_LowerCase;
-    procedure Gen_ProxyClass_DataSetAccessAsComment;
-    procedure Gen_ProxyClass_FullDataSetAccess;
+    procedure GenClass_DataSet_Nil;
+    procedure GenClass_Dataset_OneInteger;
+    procedure GenClass_TwoFields_LowerCaseStyle;
+    procedure GenClass_AccessToDataSet_InComments;
+    procedure GenClass_AccessToDataSet_Full;
     // ---
-    procedure Test_MethodConnectFields_DataSetNil;
-    procedure Test_MethodConnectFields_DataSetOneField;
-    procedure Gen_MethodConnectFields_TwoFields_LowerCase;
+    procedure GenMethod_ConnectFields_DataSet_Nil;
+    procedure GenMethod_ConnectFields_DataSet_OneString;
+    procedure GenMethod_ConnectFields_TwoFields_LowerCaseStyle;
   end;
 
 implementation
@@ -99,13 +101,13 @@ end;
 // Tests: Unit Header / Uses Section
 // -----------------------------------------------------------------------
 
-procedure TestGenerator.Test_UnitHeader_IsEmpty;
+procedure TestGenerator.GenUnitHeader_IsEmpty;
 begin
   fGenerator.Generate_UnitHeader;
   Assert.AreEqual('', fGenerator.Code.Text);
 end;
 
-procedure TestGenerator.Test_UsesSection;
+procedure TestGenerator.GenUsesSection;
 var
   actualCode: string;
 begin
@@ -163,12 +165,48 @@ begin
 end;
 
 
+// -----------------------------------------------------------------------
+// Tests: Property generation in class definition
+// -----------------------------------------------------------------------
+
+procedure TestGenerator.GenProperty_Date;
+var
+  fld: TField;
+  actualCode: string;
+begin
+  fld := GivenField(fOwner, 'BirthDate', ftDate);
+
+  actualCode := fGenerator.Generate_PublicProperty(fld);
+
+  Assert.AreEqual('property BirthDate :TDateField read FBirthDate;',
+    actualCode);
+end;
+
+
+// -----------------------------------------------------------------------
+// Tests: Generate one private field assigment
+// -----------------------------------------------------------------------
+
+procedure TestGenerator.GenFieldAssigment_Currency;
+var
+  fld: TField;
+  actualCode: string;
+begin
+  fld := GivenField(fOwner, 'Budget', ftCurrency);
+
+  actualCode := fGenerator.Generate_FieldAssigment(fld);
+
+  Assert.AreEqual
+    ('FBudget := FDataSet.FieldByName(''Budget'') as TCurrencyField;',
+    actualCode);
+end;
+
 
 // -----------------------------------------------------------------------
 // Tests: Class Declaration
 // -----------------------------------------------------------------------
 
-procedure TestGenerator.Test_ClassDeclaration_DataSetNil;
+procedure TestGenerator.GenClass_DataSet_Nil;
 var
   actualCode: string;
 begin
@@ -187,7 +225,7 @@ begin
     (* *) '  end;'#13#10, actualCode);
 end;
 
-procedure TestGenerator.Test_ClassDeclaration_DataSetOneField;
+procedure TestGenerator.GenClass_Dataset_OneInteger;
 var
   actualCode: string;
 begin
@@ -208,7 +246,7 @@ begin
     (* *) '  end;'#13#10, actualCode);
 end;
 
-procedure TestGenerator.GenerateClass_TwoFields_LowerCase;
+procedure TestGenerator.GenClass_TwoFields_LowerCaseStyle;
 var
   actualCode: string;
 begin
@@ -233,7 +271,7 @@ begin
     (* *) '  end;'#13#10, actualCode, false);
 end;
 
-procedure TestGenerator.Gen_ProxyClass_DataSetAccessAsComment;
+procedure TestGenerator.GenClass_AccessToDataSet_InComments;
 var
   actualCode: string;
 begin
@@ -257,7 +295,7 @@ begin
     (* *) + '  end;'#13#10, actualCode, false);
 end;
 
-procedure TestGenerator.Gen_ProxyClass_FullDataSetAccess;
+procedure TestGenerator.GenClass_AccessToDataSet_Full;
 var
   actualCode: string;
 begin
@@ -285,7 +323,7 @@ end;
 // Tests: Method ConnectFields
 // -----------------------------------------------------------------------
 
-procedure TestGenerator.Test_MethodConnectFields_DataSetNil;
+procedure TestGenerator.GenMethod_ConnectFields_DataSet_Nil;
 var
   actualCode: string;
 begin
@@ -302,11 +340,11 @@ begin
     (* *) 'end;'#13#10, actualCode);
 end;
 
-procedure TestGenerator.Test_MethodConnectFields_DataSetOneField;
+procedure TestGenerator.GenMethod_ConnectFields_DataSet_OneString;
 var
   actualCode: string;
 begin
-  fGenerator.DataSet := GivenDataset([['FieldInteger', ftInteger]]);
+  fGenerator.DataSet := GivenDataset([['Fullname', ftString]]);
 
   actualCode := fGenerator.Generate_MethodConnectFields;
 
@@ -315,12 +353,12 @@ begin
     (* *) 'const'#13#10 +
     (* *) '  ExpectedFieldCount = 1;'#13#10 +
     (* *) 'begin'#13#10 +
-    (* *) '  FFieldInteger := FDataSet.FieldByName(''FieldInteger'') as TIntegerField;'#13#10
+    (* *) '  FFullName := FDataSet.FieldByName(''FullName'') as TStringField;'#13#10
     (* *) + '  Assert(FDataSet.Fields.Count = ExpectedFieldCount);'#13#10 +
     (* *) 'end;'#13#10, actualCode);
 end;
 
-procedure TestGenerator.Gen_MethodConnectFields_TwoFields_LowerCase;
+procedure TestGenerator.GenMethod_ConnectFields_TwoFields_LowerCaseStyle;
 var
   actualCode: string;
 begin
