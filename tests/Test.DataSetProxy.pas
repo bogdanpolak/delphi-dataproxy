dbc1022633e501edde6abcf4ce971ff682ff8bf7
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-01-21 20:04:20 +0000

    Added tests for TBookProxy

---
+++ tests/Test.DataSetProxy.pas
@@ -0,0 +1,137 @@
+unit Test.DataSetProxy;
+
+interface
+
+uses
+  DUnitX.TestFramework,
+  System.Classes,
+  System.SysUtils,
+  System.Variants,
+  Data.DB,
+
+  Data.DataProxy;
+
+{$M+}
+
+type
+
+  [TestFixture]
+  TestBookMemProxy = class(TObject)
+  private
+    fOwner: TComponent;
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+  published
+    procedure Locate_BookTitle;
+  end;
+
+implementation
+
+uses
+  Datasnap.DBClient;
+
+// -----------------------------------------------------------------------
+// DataSetProxy factories
+// -----------------------------------------------------------------------
+
+type
+  TBookProxy = class(TDatasetProxy)
+  private
+    FISBN: TWideStringField;
+    FTitle: TWideStringField;
+    FAuthor: TWideStringField;
+    FReleseDate: TDateField;
+    FPages: TIntegerField;
+    FPrice: TCurrencyField;
+  protected
+    procedure ConnectFields; override;
+  public
+    property ISBN: TWideStringField read FISBN;
+    property Title: TWideStringField read FTitle;
+    property Author: TWideStringField read FAuthor;
+    property ReleseDate: TDateField read FReleseDate;
+    property Pages: TIntegerField read FPages;
+    property Price: TCurrencyField read FPrice;
+  end;
+
+procedure TBookProxy.ConnectFields;
+begin
+  FISBN := FDataSet.FieldByName('ISBN') as TWideStringField;
+  FTitle := FDataSet.FieldByName('Title') as TWideStringField;
+  FAuthor := FDataSet.FieldByName('Author') as TWideStringField;
+  FReleseDate := FDataSet.FieldByName('ReleseDate') as TDateField;
+  FPages := FDataSet.FieldByName('Pages') as TIntegerField;
+  FPrice := FDataSet.FieldByName('Price') as TCurrencyField;
+end;
+
+function GivenBookDataSet (aOwner: TComponent): TDataSet;
+var
+  ds: TClientDataSet;
+begin
+  ds := TClientDataSet.Create(aOwner);
+  with ds do
+  begin
+    FieldDefs.Add('ISBN', ftWideString, 15);
+    FieldDefs.Add('Title', ftWideString, 150);
+    FieldDefs.Add('Author', ftWideString, 100);
+    FieldDefs.Add('ReleseDate', ftDate);
+    FieldDefs.Add('Pages', ftInteger);
+    FieldDefs.Add('Price', ftCurrency);
+    CreateDataSet;
+    AppendRecord(['978-0201633610',
+      'Design Patterns: Elements of Reusable Object-Oriented Software',
+      'Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides',
+      EncodeDate(1994, 11, 1), 395, 54.90]);
+    AppendRecord(['978-0201485677',
+      'Refactoring: Improving the Design of Existing Code',
+      'Martin Fowler,' + ' Kent Beck,' + ' John Brant,' + ' William Opdyke,' +
+      ' Don Roberts', EncodeDate(1999, 7, 1), 464, 52.98]);
+    AppendRecord(['978-0131177055', 'Working Effectively with Legacy Code',
+      'Michael Feathers', EncodeDate(2004, 10, 1), 464, 52.69]);
+    AppendRecord(['978-0321127426',
+      'Patterns of Enterprise Application Architecture', 'Martin Fowler',
+      EncodeDate(2002, 11, 1), 560, 55.99]);
+  end;
+  ds.First;
+  Result := ds;
+end;
+
+function Given_BookDasetProxy(aOwner: TComponent): TBookProxy;
+begin
+  Result := TBookProxy.Create(aOwner)
+  (* *) .WithDataSet(GivenBookDataSet(aOwner)) as TBookProxy;
+end;
+
+// -----------------------------------------------------------------------
+// Setup and TearDown section
+// -----------------------------------------------------------------------
+
+procedure TestBookMemProxy.Setup;
+begin
+  fOwner := TComponent.Create(nil);
+end;
+
+procedure TestBookMemProxy.TearDown;
+begin
+  fOwner.Free;
+end;
+
+// -----------------------------------------------------------------------
+// Tests: Locate
+// -----------------------------------------------------------------------
+
+procedure TestBookMemProxy.Locate_BookTitle;
+var
+  aBookProxy: TBookProxy;
+begin
+  aBookProxy := Given_BookDasetProxy(fOwner);
+
+  aBookProxy.Locate('Title', 'Working Effectively with Legacy Code', []);
+
+  Assert.AreEqual('Michael Feathers', aBookProxy.Author.Value);
+end;
+
+end.
