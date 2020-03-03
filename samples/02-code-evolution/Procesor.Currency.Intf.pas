dae071f723b64f98d76f43d323d9308110735d37
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-03-03 00:07:10 +0000

    Implemented TCurrencyProcessor

--- samples/02-code-evolution/Procesor.Currency.Intf.pas
+++ samples/02-code-evolution/Procesor.Currency.Intf.pas
@@ -3,8 +3,18 @@ unit Procesor.Currency.Intf;
 interface
 
 type
+  TCurrencyRate = record
+    Code: String;
+    Rate: Currency;
+  end;
+
   ICurrencyProcessor = interface(IInvokable)
     ['{08CE1219-BC8A-45C9-B704-9D8D3B25FDFB}']
+    function IsInitialiased: boolean;
+    procedure Download(const aURL: string);
+    function IsCurrencySupported(const aCode: string): boolean;
+    function Convert(aValue: Currency; const aFromCurrency: string;
+      const aToCurrency: string): Currency;
   end;
 
 implementation
